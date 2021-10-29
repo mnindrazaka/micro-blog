465aeed0c3a2620e6ccaf098b9fe5ebd9c5dcbaa
M. Nindra Zaka <nindra.zaka@ruangguru.com>
2021-10-29 20:13:55 +0700

    feat: implement event sync

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,8 +5,12 @@ const axios = require("axios").default;
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post("/events", (req, res) => {
   const event = req.body;
+
+  events.push(event);
 
   axios
     .post("http://localhost:4000/events", event)
@@ -17,8 +21,15 @@ app.post("/events", (req, res) => {
   axios
     .post("http://localhost:4002/events", event)
     .catch((err) => console.log(err));
+  axios
+    .post("http://localhost:4003/events", event)
+    .catch((err) => console.log(err));
 
   res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
